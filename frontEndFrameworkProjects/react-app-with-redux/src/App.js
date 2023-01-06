383fcb47f93e91ac9a07655a942b2241b5177a51
Mohit <mohit.patel1966@gmail.com>
2023-01-06 19:22:39 +0530

    setup is working

--- frontEndFrameworkProjects/react-app-with-redux/src/App.js
+++ frontEndFrameworkProjects/react-app-with-redux/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
 
-  // const quotes =["qoute1","qoute2","qoute3","qoute4","qoute5","qoute6"];
+  const quotes =["qoute1","qoute2","qoute3","qoute4","qoute5","qoute6"];
   // const getQuotes = async () => {
   //   const quote = await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json");
   //   return quote;
