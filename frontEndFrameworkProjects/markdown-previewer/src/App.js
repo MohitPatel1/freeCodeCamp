680dc54520f3aaf5c29eecd82c418e3377fa171b
Mohit <mohit.patel1966@gmail.com>
2023-01-11 23:34:51 +0530

    markdown previewer

---
+++ frontEndFrameworkProjects/markdown-previewer/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+          <h1 className="text-center">Markdown Editor</h1>
+          <div className="flex w-full">
+            <h2 className=""></h2>
+            <h2 className=""></h2>
+
+          </div>
+      </div>
+    );
+  }
+}
